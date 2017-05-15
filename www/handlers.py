93c9c7a39729120934e06a441f3d862f66c48d78
jinglego <cjj3027@qq.com>
2017-05-15 23:25:30 +0800

    add static and templates

--- www/handlers.py
+++ www/handlers.py
@@ -9,6 +9,7 @@ from coroweb import get, post
 
 from models import User, Comment, Blog, next_id
 
+'''
 @get('/')
 async def index(request):
     users = await User.findAll()
@@ -16,3 +17,18 @@ async def index(request):
         '__template__': 'test.html',
         'users': users
     }
+'''
+
+@get('/')
+def index(request):
+	summary = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+	blogs = [
+	    Blog(id='1', name='Test Blog', summary=summary, created_at=time.time()-120),
+	    Blog(id='2', name='Something New', summary=summary, created_at=time.time()-3600),
+	    Blog(id='3', name='Learn Swift', summary=summary, created_at=time.time()-7200)
+	]
+	return {
+	    '__template__': 'blogs.html',
+	    'blogs': blogs
+	}
+
